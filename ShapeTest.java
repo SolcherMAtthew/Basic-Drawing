7da66603c2c67b96ee1de33a5d872c3cbe56dabc
Matthew Solcher <31741986+SolcherMAtthew@users.noreply.github.com>
2017-09-07 13:26:42 -0500

    Add files via upload

---
+++ ShapeTest.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.AWTEvent;
+import java.awt.Color;
+import java.awt.MouseInfo;
+import java.util.concurrent.TimeUnit;
+import java.awt.Toolkit;
+import java.awt.event.AWTEventListener;
+
+public class ShapeTest extends JFrame{
+   public ShapeTest(){
+      setSize(2600,2600);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setLocationRelativeTo(null);
+      setVisible(true);
+   }
+
+   public static void main(String a[]){
+      new ShapeTest();
+   }
+
+   public void paint(Graphics g){
+      while(true == true)
+      {
+      
+         try{
+            TimeUnit.SECONDS.sleep(1/8);
+           
+         
+            double mouseX = MouseInfo.getPointerInfo().getLocation().getX();
+            double mouseY = MouseInfo.getPointerInfo().getLocation().getY();
+            int X = (int)mouseX;
+            int Y = (int)mouseY;
+            Color redish = new Color(200,10,10);
+            g.setColor(redish);
+            g.fillOval(X, Y, 60, 60); 
+         }
+         catch(InterruptedException ex){
+            Thread.currentThread().interrupt();
+            
+         }
+      }
+              
+   }
+}
+
